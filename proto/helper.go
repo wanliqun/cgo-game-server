25c6cdbd35540d4be9c8b9fe6ab3e68c8c29f347
wanliqun <wanliqunn@163.com>
2023-12-16 00:36:06 +0800

    Set message type for proto request message

--- proto/helper.go
+++ proto/helper.go
@@ -33,21 +33,30 @@ func NewResponseMessage(msg proto.Message) (*Message, error) {
 }
 
 func NewRequestMessage(msg proto.Message) (*Message, error) {
+	var msgType MessageType
 	request := &Request{}
+
 	switch v := msg.(type) {
 	case *InfoRequest:
+		msgType = MessageType_INFO
 		request.Body = &Request_Info{v}
 	case *LoginRequest:
+		msgType = MessageType_LOGIN
 		request.Body = &Request_Login{v}
 	case *LogoutRequest:
+		msgType = MessageType_LOGOUT
 		request.Body = &Request_Logout{v}
 	case *GenerateRandomNicknameRequest:
+		msgType = MessageType_GENERATE_RANDOM_NICKNAME
 		request.Body = &Request_GenerateRandomNickname{v}
 	// TODO: extend for more message types support
 	default:
 		return nil, invalidProtoMessage
 	}
 
-	res := &Message{Body: &Message_Request{request}}
+	res := &Message{
+		Type: msgType,
+		Body: &Message_Request{request},
+	}
 	return res, nil
 }
